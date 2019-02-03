f87151e2621450cd6383cdea1a4e808f202d5459
VadimFarutin <vfarut@gmail.com>
2019-02-03 20:42:30 +0300

    Deleted CR character

--- homework01/src/test/java/ru/spbau/farutin/homework01/CLITest.java
+++ homework01/src/test/java/ru/spbau/farutin/homework01/CLITest.java
@@ -43,7 +43,7 @@ public class CLITest {
         cli.run();
 
         String output = byteOutputStream.toString();
-        assertThat(output, is("> 42\r\n> "));
+        assertThat(output, is("> 42\n> "));
     }
 
     /**
@@ -67,6 +67,6 @@ public class CLITest {
         cli.run();
 
         String output = byteOutputStream.toString();
-        assertThat(output, is("> exception\r\n> "));
+        assertThat(output, is("> exception\n> "));
     }
 }
