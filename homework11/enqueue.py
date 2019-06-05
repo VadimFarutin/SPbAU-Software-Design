bbd7845e4b1ab171eff392b72b490880f22dd727
VadimFarutin <vfarut@gmail.com>
2019-06-05 23:56:40 +0300

    Added DelaySeconds attribute for queue

--- homework11/enqueue.py
+++ homework11/enqueue.py
@@ -13,6 +13,6 @@ to_queue = sqs.get_queue_by_name(QueueName=to_queue_name)
 while True:
     for message in from_queue.receive_messages():
         value = int(message.body)
-        message.delete()
         print("%s received %d" % (from_queue_name, value), flush=True)
         to_queue.send_message(MessageBody=str(value + 1))
+        message.delete()
--- homework11/init.py
+++ homework11/init.py
@@ -4,6 +4,6 @@ from time import sleep
 sleep(10)
 sqs = boto3.resource('sqs', endpoint_url='http://localstack:4576/')
 
-queue = sqs.create_queue(QueueName="A")
-queue = sqs.create_queue(QueueName="B")
+queue = sqs.create_queue(QueueName="A", Attributes={'DelaySeconds': '3'})
+queue = sqs.create_queue(QueueName="B", Attributes={'DelaySeconds': '3'})
 queue.send_message(MessageBody="1")
